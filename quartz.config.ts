12c4987f7cddecaf47ab4ca645e212aae7d05c52
saku0903hu <atria0903@gmail.com>
2025-12-30 03:10:05 +0900

    seeds and pagetitle change

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Physical Gardens",
+    pageTitle: "Quanta Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
